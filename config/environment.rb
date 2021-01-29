c33072f004d24d18155722d19beba7c90bf58eb5
Eduardo Borba <eduardo@jobscore.com>
2021-01-29 10:07:55 -0300

    Remove unnecessary line

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,6 @@ require "rubygems"
 require "bundler"
 Bundler.require(:default)                   # load all the default gems
 Bundler.require(Sinatra::Base.environment)  # load all the environment specific gems
-Dir.glob('./{app,models}/*.rb').each { |file| require file }
 require './app/book_api.rb'
 require './models/book.rb'
 require './serializers/book_serializer.rb'
