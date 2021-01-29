0f700dd1b7eec542812dfcfc7d02945b506dd487
Eduardo Borba <eduardo@jobscore.com>
2021-01-28 22:12:33 -0300

    Separate models and serializers from app

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,9 @@ require "rubygems"
 require "bundler"
 Bundler.require(:default)                   # load all the default gems
 Bundler.require(Sinatra::Base.environment)  # load all the environment specific gems
-Dir.glob('./{app}/*.rb').each { |file| require file }
+Dir.glob('./{app,models}/*.rb').each { |file| require file }
+require './app/book_api.rb'
+require './models/book.rb'
+require './serializers/book_serializer.rb'
 
 Mongoid.load! 'mongoid.config'
